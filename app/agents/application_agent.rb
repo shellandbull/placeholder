25c18071e019505d73d38f3b3f286cd190239a8f
Mario Gintili <mariojgintili@gmail.com>
2025-09-22 16:37:49 +0100

    tutorial

--- app/agents/application_agent.rb
+++ app/agents/application_agent.rb
@@ -1,6 +1,5 @@
 class ApplicationAgent < ActiveAgent::Base
   layout "agent"
 
-  generate_with :openai, model: "gpt-4o-mini", instructions: "You are a helpful assistant."
+  generate_with :ollama, model: "gemma3:latest", instructions: "You are a helpful assistant."
 end
-
